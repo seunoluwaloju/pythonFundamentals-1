78a50e6179a751dd031c77d3632a2a2fd8fc19ec
seunoluwaloju <sakintim@thoughtworks.com>
2021-04-05 21:05:23 -0500

    variables python lab code

--- variables.py
+++ variables.py
@@ -1,2 +1,17 @@
 #!/usr/local/bin/python3
-print("Hello John")
+
+# int
+num = 1
+print(num + 20)
+num = num + 30
+print(num)
+
+# float
+num2 = 0.5
+num2 = num2 + 10
+print(num2 + 10.5)
+
+# string
+name = 'Peter'
+print(f'{name} is his name')
+name = name + "john"
